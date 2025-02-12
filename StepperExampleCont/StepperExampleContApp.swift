40b7098544cb05512129d84f3fab4a338f86a75e
Jack Rathwell <jrathwell@lcs.on.ca>
2025-02-12 09:05:03 -0500

    Initial Commit

---
+++ StepperExampleCont/StepperExampleContApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleContApp.swift
+//  StepperExampleCont
+//
+//  Created by Jack William Rathwell on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleContApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
